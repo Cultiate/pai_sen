417ed5032050ef5c1cf4714bb92b07e20fb36461
Yusuke Tachibana <yusuke3627@gmail.com>
2018-12-16 20:32:56 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root "root#top"
-  get 'tests/test'
+
+  get  "student_signup" => "students#new"
+  post  "student_signup" => "students#create"
+  get "student_send_email" => "students#send_email"
+  get "mypage/:student_id" => "students#show", as: :student_mypage
+
+  get  "coach_signup" => "coachs#new"
+  post  "coach_signup" => "coachs#create"
+  get "coach_send_email" => "coachs#send_email"
+  get "mypage/:coach_id" => "coachs#show", as: :coach_mypage
 end
